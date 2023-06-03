97f2b8e3803f8a2c050705314ed28c982ace8955
rana782 <ranajnv782@gmail.com>
2023-06-03 17:41:40 +0000

    LeetCode: Sum Multiples (sum-multiples) — placeholder (solved on 2023-06-03T17:41:40Z)

--- sum-multiples/sum-multiples.java
+++ sum-multiples/sum-multiples.java
@@ -1,16 +1,6 @@
-// LeetCode: Sum Multiples (sum-multiples)
-// Submission ID: 963122459
-// Language: java
-// Timestamp (UTC): 2023-06-03T17:41:40Z
+# Placeholder for Sum Multiples (sum-multiples)
 
-class Solution {
-    public int sumOfMultiples(int n) {
-        int sum =0;
-        for(int i =1; i<=n ; i++){
-            if(i%3==0 || i%5==0 || i%7==0){
-                sum+=i;
-            }
-        }
-        return sum;
-    }
-}
+- **Submission ID:** 963122459
+- **Original solve time (UTC):** 2023-06-03T17:41:40Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
