44a75ee6558925b046a216466fd7c811306ca62f
rana782 <ranajnv782@gmail.com>
2023-06-04 10:02:55 +0000

    LeetCode: Maximum Sum With Exactly K Elements  (maximum-sum-with-exactly-k-elements) — placeholder (solved on 2023-06-04T10:02:55Z)

--- maximum-sum-with-exactly-k-elements/maximum-sum-with-exactly-k-elements.java
+++ maximum-sum-with-exactly-k-elements/maximum-sum-with-exactly-k-elements.java
@@ -1,16 +1,6 @@
-// LeetCode: Maximum Sum With Exactly K Elements  (maximum-sum-with-exactly-k-elements)
-// Submission ID: 963599074
-// Language: java
-// Timestamp (UTC): 2023-06-04T10:02:55Z
+# Placeholder for Maximum Sum With Exactly K Elements  (maximum-sum-with-exactly-k-elements)
 
-class Solution {
-    public int maximizeSum(int[] nums, int k) {
-        Arrays.sort(nums);
-        int sum = 0;
-        for(int i = 0; i<k; i++){
-             sum+=nums[nums.length-1];
-             nums[nums.length-1] = ++nums[nums.length-1];
-        }
-        return sum;
-    }
-}
+- **Submission ID:** 963599074
+- **Original solve time (UTC):** 2023-06-04T10:02:55Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
