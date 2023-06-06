3a48bfee2ec6662b21bfa472468d7945d003b301
rana782 <ranajnv782@gmail.com>
2023-06-06 10:10:02 +0000

    LeetCode: Merge Nodes in Between Zeros (merge-nodes-in-between-zeros) — placeholder (solved on 2023-06-06T10:10:02Z)

--- merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
+++ merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
@@ -1,41 +1,6 @@
-// LeetCode: Merge Nodes in Between Zeros (merge-nodes-in-between-zeros)
-// Submission ID: 965064475
-// Language: java
-// Timestamp (UTC): 2023-06-06T10:10:02Z
+# Placeholder for Merge Nodes in Between Zeros (merge-nodes-in-between-zeros)
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode mergeNodes(ListNode head) {
-        if(head==null || head.next == null){
-            return head;
-        }
-     ListNode find = new ListNode(0);
-     
-     ListNode tail = find; 
-     ListNode temp = head;
-     
-     while(temp!=null){
-         int sum = 0;
-         while(temp.val!=0){
-           sum+=temp.val;
-           temp = temp.next;
-         }
-         temp = temp.next;
-         ListNode store = new ListNode(sum);
-         tail.next = store;
-         tail = store;
-     }
-     
-     System.gc();
-     return find.next.next;
-    }
-}
+- **Submission ID:** 965064475
+- **Original solve time (UTC):** 2023-06-06T10:10:02Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
