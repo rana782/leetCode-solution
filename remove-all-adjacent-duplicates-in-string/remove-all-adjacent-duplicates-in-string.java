65589a40b64b9835b3ef3d99359593e81072d096
rana782 <ranajnv782@gmail.com>
2023-07-13 05:29:20 +0000

    LeetCode: Remove All Adjacent Duplicates In String (remove-all-adjacent-duplicates-in-string) — placeholder (solved on 2023-07-13T05:29:20Z)

--- remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,30 +1,6 @@
-// LeetCode: Remove All Adjacent Duplicates In String (remove-all-adjacent-duplicates-in-string)
-// Submission ID: 993202934
-// Language: java
-// Timestamp (UTC): 2023-07-13T05:29:20Z
+# Placeholder for Remove All Adjacent Duplicates In String (remove-all-adjacent-duplicates-in-string)
 
-class Solution {
-    public String removeDuplicates(String s) {
-        Stack<Character> st = new Stack<>();
-        for(int i =0; i<s.length(); i++){
-            if(st.empty()){
-                st.push(s.charAt(i));
-            }
-            else if(st.peek() == s.charAt(i)){
-                st.pop();
-            }
-            else{
-                st.push(s.charAt(i));
-            }
-        }
-          StringBuilder ans= new StringBuilder();
-
-        while(!st.isEmpty()){
-            ans.append( st.pop());
-        }
-
-
-        // reverse it as stack is first in last out
-        return ans.reverse().toString() ;
-    }
-}
+- **Submission ID:** 993202934
+- **Original solve time (UTC):** 2023-07-13T05:29:20Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
