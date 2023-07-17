82c38a9f703f81c9bc51158a909ebd5905f0b3e3
rana782 <ranajnv782@gmail.com>
2023-07-17 14:16:45 +0000

    LeetCode: Root Equals Sum of Children (root-equals-sum-of-children) — placeholder (solved on 2023-07-17T14:16:45Z)

--- root-equals-sum-of-children/root-equals-sum-of-children.java
+++ root-equals-sum-of-children/root-equals-sum-of-children.java
@@ -1,28 +1,6 @@
-// LeetCode: Root Equals Sum of Children (root-equals-sum-of-children)
-// Submission ID: 996741137
-// Language: java
-// Timestamp (UTC): 2023-07-17T14:16:45Z
+# Placeholder for Root Equals Sum of Children (root-equals-sum-of-children)
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean checkTree(TreeNode root) {
-        if(root.val == root.left.val+root.right.val){
-            return true;
-        }
-        return false;
-    }
-}
+- **Submission ID:** 996741137
+- **Original solve time (UTC):** 2023-07-17T14:16:45Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
