c101696dc6c69ec13108cc479486ae45ffef71aa
rana782 <ranajnv782@gmail.com>
2024-07-27 11:05:39 +0000

    LeetCode: Number of Good Pairs (number-of-good-pairs) — placeholder (solved on 2024-07-27T11:05:39Z)

--- number-of-good-pairs/number-of-good-pairs.java
+++ number-of-good-pairs/number-of-good-pairs.java
@@ -1,18 +1,6 @@
-// LeetCode: Number of Good Pairs (number-of-good-pairs)
-// Submission ID: 1335012643
-// Language: java
-// Timestamp (UTC): 2024-07-27T11:05:39Z
+# Placeholder for Number of Good Pairs (number-of-good-pairs)
 
-class Solution {
-    public int numIdenticalPairs(int[] nums) {
-        int ans = 0;
-         for(int i =0; i<nums.length-1; i++){
-            for(int j =i+1; j<nums.length; j++){
-                if(nums[i] == nums[j]){
-                    ans++;
-                }
-            }
-         }
-         return ans;
-    }
-}
+- **Submission ID:** 1335012643
+- **Original solve time (UTC):** 2024-07-27T11:05:39Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
