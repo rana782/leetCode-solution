8d982a21b015070ea6cedbb4fbf64b838f246d0a
rana782 <ranajnv782@gmail.com>
2024-08-05 10:18:19 +0000

    LeetCode: Is Subsequence (is-subsequence) — placeholder (solved on 2024-08-05T10:18:19Z)

--- is-subsequence/is-subsequence.java
+++ is-subsequence/is-subsequence.java
@@ -1,30 +1,6 @@
-// LeetCode: Is Subsequence (is-subsequence)
-// Submission ID: 1345223970
-// Language: java
-// Timestamp (UTC): 2024-08-05T10:18:19Z
+# Placeholder for Is Subsequence (is-subsequence)
 
-class Solution {
-    public boolean isSubsequence(String s, String t) {
-      if(s.length()>t.length()){
-        return false;
-      }
-      int j = 0;
-      int i = 0;
-     while(i<s.length() &&j<t.length()){
- if(s.charAt(i) == t.charAt(j)){
-            i++;
-            j++;
-        }
-        else{
-            j++;
-        }
-     }
-       
-      
- if(i == s.length()){
-        return true;
-      }
-      return false;
-     
-    }
-}
+- **Submission ID:** 1345223970
+- **Original solve time (UTC):** 2024-08-05T10:18:19Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
