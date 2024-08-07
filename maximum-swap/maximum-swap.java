9fbd12369b4da04a40e0925ccf89a8777a9dadc4
rana782 <ranajnv782@gmail.com>
2024-08-07 14:13:37 +0000

    LeetCode: Maximum Swap (maximum-swap) — placeholder (solved on 2024-08-07T14:13:37Z)

--- maximum-swap/maximum-swap.java
+++ maximum-swap/maximum-swap.java
@@ -1,49 +1,6 @@
-// LeetCode: Maximum Swap (maximum-swap)
-// Submission ID: 1347807591
-// Language: java
-// Timestamp (UTC): 2024-08-07T14:13:37Z
+# Placeholder for Maximum Swap (maximum-swap)
 
-class Solution {
-
-    public static void swap(char[] arr, int i, int j){
-        char ch = arr[i];
-        arr[i] = arr[j];
-        arr[j] = ch;
-    }
-    public int maximumSwap(int num) {
-        if(num>=0 && num<=11){
-            return num;
-        }
-      int lastIndex[] = new int[10];
-      String str = Integer.toString(num);
-      char[] arr = str.toCharArray();
-
-      boolean flag = false;
-
-      for(int i = 0 ;i<arr.length; i++){
-      
-        lastIndex[arr[i]-'0'] = i;
-      }
-
-      for(int i = 0; i<arr.length; i++){
-        for(int j = 9; j>arr[i]-'0'; j--){
-            if(lastIndex[j]>i){
-                flag = true;
-               
-                swap(arr,lastIndex[j],i);
-                break;
-            }
-        }
-        if(flag){
-            break;
-        }
-      }
-int ans = 0;
-for(int i = 0; i<arr.length; i++){
-    int digit = arr[i]-'0';
-    ans = ans*10+digit;
-}
-
-return ans;
-    }
-}
+- **Submission ID:** 1347807591
+- **Original solve time (UTC):** 2024-08-07T14:13:37Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
