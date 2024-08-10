5f89ff6c316a3f2229d2d19e661b8b82e9c81ff1
rana782 <ranajnv782@gmail.com>
2024-08-10 16:49:03 +0000

    LeetCode: Maximum Number of Pairs in Array (maximum-number-of-pairs-in-array) — placeholder (solved on 2024-08-10T16:49:03Z)

--- maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.java
+++ maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.java
@@ -1,32 +1,6 @@
-// LeetCode: Maximum Number of Pairs in Array (maximum-number-of-pairs-in-array)
-// Submission ID: 1351160836
-// Language: java
-// Timestamp (UTC): 2024-08-10T16:49:03Z
+# Placeholder for Maximum Number of Pairs in Array (maximum-number-of-pairs-in-array)
 
-class Solution {
-    public int[] numberOfPairs(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-		        for(int i : nums) {
-		        	if(map.containsKey(i)) {
-		        		map.put(i,map.get(i)+1);
-		        	}
-		        	else {
-		        		map.put(i,1);
-		        	}
-		        }
-		        int count = 0;
-		        for(int i : nums) {
-		        	if(map.containsKey(i)&&map.get(i)>=2) {
-		        		count++;
-		        		map.put(i,map.get(i)-2);
-		        	}
-		        }
-		        
-		        int single = nums.length-2*count;
-		        int ans[] = new int[2];
-		        
-		        ans[0] = count;
-		        ans[1] = single;
-		        return ans; 
-    }
-}
+- **Submission ID:** 1351160836
+- **Original solve time (UTC):** 2024-08-10T16:49:03Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
