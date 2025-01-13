55a2b05b9252091d454885c8a97b6df923e40f25
rana782 <ranajnv782@gmail.com>
2025-01-13 10:33:14 +0000

    LeetCode: 2 Keys Keyboard (2-keys-keyboard) — solved on 2025-01-13T10:33:14Z

--- 2-keys-keyboard/2-keys-keyboard.java
+++ 2-keys-keyboard/2-keys-keyboard.java
@@ -1,6 +1,22 @@
-# Placeholder for 2 Keys Keyboard (2-keys-keyboard)
+// LeetCode: 2 Keys Keyboard (2-keys-keyboard)
+// Submission ID: 1507141006
+// Language: java
+// Timestamp (UTC): 2025-01-13T10:33:14Z
 
-- **Submission ID:** 1507141006
-- **Original solve time (UTC):** 2025-01-13T10:33:14Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int minSteps(int n) {
+        int ans = 0;
+        for(int i = 2; i*i<=n;){
+            if(n%i == 0){
+                ans+=i;
+                n= n/i;
+            }
+            else{
+                i++;
+            }
+        }
+
+        if(n!=1) ans+=n;
+        return ans;
+    }
+}
