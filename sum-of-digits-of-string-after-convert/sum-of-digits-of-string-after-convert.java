03c210e214e1fe522dff6759174ec0035a9ac85a
rana782 <ranajnv782@gmail.com>
2025-01-13 11:03:08 +0000

    LeetCode: Sum of Digits of String After Convert (sum-of-digits-of-string-after-convert) — solved on 2025-01-13T11:03:08Z

--- sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
+++ sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
@@ -1,6 +1,34 @@
-# Placeholder for Sum of Digits of String After Convert (sum-of-digits-of-string-after-convert)
+// LeetCode: Sum of Digits of String After Convert (sum-of-digits-of-string-after-convert)
+// Submission ID: 1507161250
+// Language: java
+// Timestamp (UTC): 2025-01-13T11:03:08Z
 
-- **Submission ID:** 1507161250
-- **Original solve time (UTC):** 2025-01-13T11:03:08Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int getLucky(String s, int k) {
+	        
+		  
+		  int value = 0;
+		  
+		  for(int i = 0; i<s.length(); i++) {
+			  char ch = s.charAt(i);
+			  value+=getSum(ch-'a'+1);
+		  }
+		  
+		  while(k!=1) {
+			  value = getSum(value);
+			  k--;
+		  }
+		  
+		  return value;
+	    }
+
+	private int getSum(int value) {
+		int sum = 0;
+		while(value!=0) {
+			sum+=value%10;
+			value = value/10;
+		}
+		
+		return sum;
+	}
+}
