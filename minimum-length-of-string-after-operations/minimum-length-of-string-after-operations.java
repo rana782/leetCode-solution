b19af846ab808b960c876999b8b1d758f0d9d999
rana782 <ranajnv782@gmail.com>
2025-01-13 08:48:34 +0000

    LeetCode: Minimum Length of String After Operations (minimum-length-of-string-after-operations) — solved on 2025-01-13T08:48:34Z

--- minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
+++ minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -1,6 +1,25 @@
-# Placeholder for Minimum Length of String After Operations (minimum-length-of-string-after-operations)
+// LeetCode: Minimum Length of String After Operations (minimum-length-of-string-after-operations)
+// Submission ID: 1507066328
+// Language: java
+// Timestamp (UTC): 2025-01-13T08:48:34Z
 
-- **Submission ID:** 1507066328
-- **Original solve time (UTC):** 2025-01-13T08:48:34Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int minimumLength(String s) {
+		HashMap<Character,Integer> map = new HashMap<>();
+	        for(int i = 0; i<s.length(); i++) {
+	        	char ch = s.charAt(i);
+	        	map.put(ch,map.getOrDefault(ch,0)+1);
+	        }
+	        
+	        int outputLength = 0;
+	        
+	        for(int i :map.values()) {
+	        	if(i%2 == 0) outputLength+=2;
+                else outputLength+=1;
+	        }
+	        
+	        return outputLength;
+	    }
+
+	
+}
