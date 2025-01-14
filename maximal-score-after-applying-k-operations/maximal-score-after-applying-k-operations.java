9031db5bce04b0a2f0521d964ae8029f82ab67d8
rana782 <ranajnv782@gmail.com>
2025-01-14 07:27:41 +0000

    LeetCode: Maximal Score After Applying K Operations (maximal-score-after-applying-k-operations) — solved on 2025-01-14T07:27:41Z

--- maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
+++ maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
@@ -1,6 +1,24 @@
-# Placeholder for Maximal Score After Applying K Operations (maximal-score-after-applying-k-operations)
+// LeetCode: Maximal Score After Applying K Operations (maximal-score-after-applying-k-operations)
+// Submission ID: 1508086655
+// Language: java
+// Timestamp (UTC): 2025-01-14T07:27:41Z
 
-- **Submission ID:** 1508086655
-- **Original solve time (UTC):** 2025-01-14T07:27:41Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public long maxKelements(int[] nums, int k) {
+	        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
+	        long maxScore = 0;
+	        for(int i : nums) {
+	        	pq.add((long)i);
+	        }
+	        
+	        while(k!=0 && pq.size()!=0) {
+	        	long removed = pq.poll();
+	        	maxScore+=removed;
+	        	long insert = (long) Math.ceil(removed/3.0);
+	        	pq.add(insert);
+	        	k--;
+	        }
+	        
+	        return maxScore;
+	    }
+}
