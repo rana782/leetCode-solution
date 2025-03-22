eb6cc152dd971d24d131a4f3f714ba0d985670da
rana782 <ranajnv782@gmail.com>
2025-03-22 07:03:06 +0000

    LeetCode: Minimum Changes To Make Alternating Binary String (minimum-changes-to-make-alternating-binary-string) — solved on 2025-03-22T07:03:06Z

--- minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -1,6 +1,35 @@
-# Placeholder for Minimum Changes To Make Alternating Binary String (minimum-changes-to-make-alternating-binary-string)
+// LeetCode: Minimum Changes To Make Alternating Binary String (minimum-changes-to-make-alternating-binary-string)
+// Submission ID: 1582033643
+// Language: java
+// Timestamp (UTC): 2025-03-22T07:03:06Z
 
-- **Submission ID:** 1582033643
-- **Original solve time (UTC):** 2025-03-22T07:03:06Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int minOperations(String s) {
+        if(s.length() == 1) return 0;
+
+        int zeroCase = 0;
+        int oneCase = 0;
+
+        for(int i = 0; i<s.length(); i++){
+            int currNum = s.charAt(i)-'0';
+
+            if(i%2 == 0 ){
+
+                if(currNum == 1) zeroCase++;
+                else{
+                    oneCase++;
+                }
+            }
+            else{
+                if(currNum == 0) zeroCase++;
+
+                else{
+                    oneCase++;
+                }
+            }
+
+        }
+
+        return Math.min(zeroCase,oneCase);
+    }
+}
