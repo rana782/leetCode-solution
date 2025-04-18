b43e75e6d2d7fe6ccb895b8fcff225fcd10233d3
rana782 <ranajnv782@gmail.com>
2025-04-18 20:10:58 +0000

    LeetCode: Best Sightseeing Pair (best-sightseeing-pair) — solved on 2025-04-18T20:10:58Z

--- best-sightseeing-pair/best-sightseeing-pair.java
+++ best-sightseeing-pair/best-sightseeing-pair.java
@@ -1,6 +1,29 @@
-# Placeholder for Best Sightseeing Pair (best-sightseeing-pair)
+// LeetCode: Best Sightseeing Pair (best-sightseeing-pair)
+// Submission ID: 1610886133
+// Language: java
+// Timestamp (UTC): 2025-04-18T20:10:58Z
 
-- **Submission ID:** 1610886133
-- **Original solve time (UTC):** 2025-04-18T20:10:58Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        int n = values.length;
+
+        int maxArr[] = new int[n];
+
+        int maxi = Integer.MIN_VALUE;
+
+        for(int i = n-1; i>0; i--){
+            maxi = Math.max(maxi,values[i]-i);
+            maxArr[i] = maxi;
+        }
+
+
+        int output = Integer.MIN_VALUE;
+
+        for(int i = 0; i<n-1; i++){
+            int currVal = values[i]+i+maxArr[i+1];
+            output = Math.max(output,currVal);
+        }
+
+        return output;
+    }
+}
