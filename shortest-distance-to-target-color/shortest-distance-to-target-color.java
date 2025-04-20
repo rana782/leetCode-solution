ca87cf9a5724376731050b3a55e883b42c29225f
rana782 <ranajnv782@gmail.com>
2025-04-20 13:49:02 +0000

    LeetCode: Shortest Distance to Target Color (shortest-distance-to-target-color) — solved on 2025-04-20T13:49:02Z

--- shortest-distance-to-target-color/shortest-distance-to-target-color.java
+++ shortest-distance-to-target-color/shortest-distance-to-target-color.java
@@ -1,6 +1,64 @@
-# Placeholder for Shortest Distance to Target Color (shortest-distance-to-target-color)
+// LeetCode: Shortest Distance to Target Color (shortest-distance-to-target-color)
+// Submission ID: 1612558323
+// Language: java
+// Timestamp (UTC): 2025-04-20T13:49:02Z
 
-- **Submission ID:** 1612558323
-- **Original solve time (UTC):** 2025-04-20T13:49:01Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public List<Integer> shortestDistanceColor(int[] colors, int[][] queries) {
+        int n = colors.length;
+
+        int leftDist[][] = new int[n][4];
+        for(int i = 0; i<n; i++){
+            Arrays.fill(leftDist[i],-1);
+        }
+
+        
+
+        for(int i = 0; i<n; i++){
+            int colr = colors[i];
+
+            leftDist[i][colr] = 0;
+
+            if(i>0){
+                     for(int col = 1; col<=3; col++){
+                if(leftDist[i-1][col]!=-1 && col!=colr)leftDist[i][col] = leftDist[i-1][col]+1;
+            }
+            }
+
+           
+        }
+
+
+        int rightDist[][] = new int[n][4];
+        for(int i = 0; i<n; i++){
+            Arrays.fill(rightDist[i],-1);
+        }
+
+        for(int i = n-1; i>=0; i--){
+
+            int colr = colors[i];
+            rightDist[i][colr] = 0;
+
+            if(i+1<n){
+
+                for(int col = 1; col<=3; col++){
+
+                    if(rightDist[i+1][col]!=-1 && col!=colr)rightDist[i][col] = rightDist[i+1][col]+1;
+                }
+            }
+        }
+
+        List<Integer>output = new ArrayList<>();
+
+        for(int i = 0; i<queries.length; i++){
+            int idx = queries[i][0];
+            int colr = queries[i][1];
+
+            int temp = Math.min(leftDist[idx][colr] == -1 ? Integer.MAX_VALUE : leftDist[idx][colr],rightDist[idx][colr] == -1 ? Integer.MAX_VALUE : rightDist[idx][colr] );
+
+            output.add(temp == Integer.MAX_VALUE ? -1 : temp);
+        }
+
+        return output;
+    }
+}
