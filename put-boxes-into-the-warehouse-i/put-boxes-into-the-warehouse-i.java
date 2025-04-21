1d15dcde41406e6f2e04c0e60f0269daaf3957a3
rana782 <ranajnv782@gmail.com>
2025-04-21 19:12:08 +0000

    LeetCode: Put Boxes Into the Warehouse I (put-boxes-into-the-warehouse-i) — solved on 2025-04-21T19:12:08Z

--- put-boxes-into-the-warehouse-i/put-boxes-into-the-warehouse-i.java
+++ put-boxes-into-the-warehouse-i/put-boxes-into-the-warehouse-i.java
@@ -1,6 +1,33 @@
-# Placeholder for Put Boxes Into the Warehouse I (put-boxes-into-the-warehouse-i)
+// LeetCode: Put Boxes Into the Warehouse I (put-boxes-into-the-warehouse-i)
+// Submission ID: 1613907726
+// Language: java
+// Timestamp (UTC): 2025-04-21T19:12:08Z
 
-- **Submission ID:** 1613907726
-- **Original solve time (UTC):** 2025-04-21T19:12:07Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int maxBoxesInWarehouse(int[] boxes, int[] warehouse) {
+        int minHeights[] = new int[warehouse.length];
+        minHeights[0] = warehouse[0];
+
+        for(int i = 1; i<warehouse.length; i++){
+            minHeights[i] = Math.min(minHeights[i-1],warehouse[i]);
+        }
+
+        int output = 0;
+
+        Arrays.sort(boxes);
+
+        int i = 0;
+        int j = warehouse.length-1;
+
+        while(i<boxes.length && j>=0){
+
+            if(boxes[i]<=minHeights[j]){
+                output++;
+                i++;
+            }
+            j--;
+        }
+
+        return output;
+    }
+}
