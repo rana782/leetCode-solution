6bea62a825450caed22b1620b2c831996b1d514c
rana782 <ranajnv782@gmail.com>
2025-04-21 14:10:54 +0000

    LeetCode: Intersection of Three Sorted Arrays (intersection-of-three-sorted-arrays) — solved on 2025-04-21T14:10:54Z

--- intersection-of-three-sorted-arrays/intersection-of-three-sorted-arrays.java
+++ intersection-of-three-sorted-arrays/intersection-of-three-sorted-arrays.java
@@ -1,6 +1,31 @@
-# Placeholder for Intersection of Three Sorted Arrays (intersection-of-three-sorted-arrays)
+// LeetCode: Intersection of Three Sorted Arrays (intersection-of-three-sorted-arrays)
+// Submission ID: 1613607957
+// Language: java
+// Timestamp (UTC): 2025-04-21T14:10:54Z
 
-- **Submission ID:** 1613607957
-- **Original solve time (UTC):** 2025-04-21T14:10:54Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        int common[] = new int[2001];
+
+        for(int i = 0; i<arr1.length; i++){
+            common[arr1[i]]++;
+        }
+
+                for(int i = 0; i<arr2.length; i++){
+            common[arr2[i]]++;
+        }
+
+                for(int i = 0; i<arr3.length; i++){
+              common[arr3[i]]++;
+        }
+
+
+        List<Integer>output = new ArrayList<>();
+
+        for(int i = 0; i<common.length; i++){
+            if(common[i] == 3)output.add(i);
+        }
+
+        return output;
+    }
+}
