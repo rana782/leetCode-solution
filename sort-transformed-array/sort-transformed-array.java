ec5eb131d747f370e1dda7425ea33ce374395171
rana782 <ranajnv782@gmail.com>
2025-04-22 20:24:40 +0000

    LeetCode: Sort Transformed Array (sort-transformed-array) — solved on 2025-04-22T20:24:40Z

--- sort-transformed-array/sort-transformed-array.java
+++ sort-transformed-array/sort-transformed-array.java
@@ -1,6 +1,25 @@
-# Placeholder for Sort Transformed Array (sort-transformed-array)
+// LeetCode: Sort Transformed Array (sort-transformed-array)
+// Submission ID: 1615003888
+// Language: java
+// Timestamp (UTC): 2025-04-22T20:24:40Z
 
-- **Submission ID:** 1615003888
-- **Original solve time (UTC):** 2025-04-22T20:24:40Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+
+    public static int fxn(int a, int b, int c, int x){
+        return (a*x*x)+(b*x)+c;
+    }
+    public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
+        int n = nums.length;
+        int output[] = new int[n];
+
+        for(int i = 0; i<nums.length; i++){
+
+            int ans = fxn(a,b,c,nums[i]);
+            output[i] = ans;
+        }
+
+        Arrays.sort(output);
+
+        return output;
+    }
+}
