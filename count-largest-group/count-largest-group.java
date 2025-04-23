11763808d693c3049f3e9a501d3b1a4756a2770f
rana782 <ranajnv782@gmail.com>
2025-04-23 11:42:01 +0000

    LeetCode: Count Largest Group (count-largest-group) — solved on 2025-04-23T11:42:01Z

--- count-largest-group/count-largest-group.java
+++ count-largest-group/count-largest-group.java
@@ -1,6 +1,40 @@
-# Placeholder for Count Largest Group (count-largest-group)
+// LeetCode: Count Largest Group (count-largest-group)
+// Submission ID: 1615607793
+// Language: java
+// Timestamp (UTC): 2025-04-23T11:42:01Z
 
-- **Submission ID:** 1615607793
-- **Original solve time (UTC):** 2025-04-23T11:42:01Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public static int getSum(int n){
+        if(n<10)return n;
+
+        int output = 0;
+
+        while(n!=0){
+            int rem = n%10;
+            output+=rem;
+            n/=10;
+        }
+
+        return output;
+    }
+    public int countLargestGroup(int n) {
+        
+        HashMap<Integer,Integer>map = new HashMap<>();
+        int maxSize = 0;
+
+        for(int i = 1; i<=n; i++){
+            int digitSum = getSum(i);
+
+            map.put(digitSum,map.getOrDefault(digitSum,0)+1);
+            maxSize = Math.max(maxSize,map.get(digitSum));
+        }
+
+        int output = 0;
+
+        for(int ele : map.values()){
+            if(ele == maxSize)output++;
+        }
+
+        return output;
+    }
+}
