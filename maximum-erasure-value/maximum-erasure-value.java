4ca967ee10c40abab443d580daf1df281b20a971
rana782 <ranajnv782@gmail.com>
2025-04-24 08:01:54 +0000

    LeetCode: Maximum Erasure Value (maximum-erasure-value) — solved on 2025-04-24T08:01:54Z

--- maximum-erasure-value/maximum-erasure-value.java
+++ maximum-erasure-value/maximum-erasure-value.java
@@ -1,6 +1,35 @@
-# Placeholder for Maximum Erasure Value (maximum-erasure-value)
+// LeetCode: Maximum Erasure Value (maximum-erasure-value)
+// Submission ID: 1616434388
+// Language: java
+// Timestamp (UTC): 2025-04-24T08:01:54Z
 
-- **Submission ID:** 1616434388
-- **Original solve time (UTC):** 2025-04-24T08:01:54Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        int n = nums.length;
+        int prefixSum[] = new int[n+1];
+        prefixSum[0] = 0;
+
+        for(int i = 1; i<=n; i++){
+                prefixSum[i] = prefixSum[i-1]+nums[i-1];
+        }
+
+        int output = 0;
+        int j = 0;
+        HashSet<Integer>set = new HashSet<>();
+
+        for(int i = 0; i<n; i++){
+           
+
+            while(j<=i && set.contains(nums[i])){
+              
+                set.remove(nums[j]);
+                j++;
+            }
+            set.add(nums[i]);
+            int currSum = prefixSum[i+1]-prefixSum[j];
+            output = Math.max(output,currSum);
+        }
+
+        return output;
+    }
+}
