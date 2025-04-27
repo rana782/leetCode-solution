4a1c0106323bd03f287caee1cd4c2ec26ff9f1cb
rana782 <ranajnv782@gmail.com>
2025-04-27 09:12:27 +0000

    LeetCode: Count Subarrays of Length Three With a Condition (count-subarrays-of-length-three-with-a-condition) — solved on 2025-04-27T09:12:27Z

--- count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.java
+++ count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.java
@@ -1,6 +1,26 @@
-# Placeholder for Count Subarrays of Length Three With a Condition (count-subarrays-of-length-three-with-a-condition)
+// LeetCode: Count Subarrays of Length Three With a Condition (count-subarrays-of-length-three-with-a-condition)
+// Submission ID: 1619184036
+// Language: java
+// Timestamp (UTC): 2025-04-27T09:12:27Z
 
-- **Submission ID:** 1619184036
-- **Original solve time (UTC):** 2025-04-27T09:12:26Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int countSubarrays(int[] nums) {
+        int output = 0;
+
+        int right = 2;
+        int left = 0;
+         int middle = 1;
+
+         while(right<nums.length){
+            if((double)(nums[right]+nums[left]) == (double)nums[middle]/2.0)output++;
+            
+
+            right++;
+            left++;
+            middle++;
+
+         }
+
+         return output;
+    }
+}
