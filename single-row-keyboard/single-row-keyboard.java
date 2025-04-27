219cd0043a41f721ba3aaee62ef1165e021851f0
rana782 <ranajnv782@gmail.com>
2025-04-27 13:31:14 +0000

    LeetCode: Single-Row Keyboard (single-row-keyboard) — solved on 2025-04-27T13:31:14Z

--- single-row-keyboard/single-row-keyboard.java
+++ single-row-keyboard/single-row-keyboard.java
@@ -1,6 +1,29 @@
-# Placeholder for Single-Row Keyboard (single-row-keyboard)
+// LeetCode: Single-Row Keyboard (single-row-keyboard)
+// Submission ID: 1619355668
+// Language: java
+// Timestamp (UTC): 2025-04-27T13:31:14Z
 
-- **Submission ID:** 1619355668
-- **Original solve time (UTC):** 2025-04-27T13:31:13Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int calculateTime(String keyboard, String word) {
+        int prevIdx = 0;
+        HashMap<Character,Integer>map = new HashMap<>();
+
+        for(int i = 0; i<keyboard.length(); i++){
+            char ch = keyboard.charAt(i);
+            map.put(ch,i);
+        }
+
+        int cost = 0;
+
+        for(int i = 0; i<word.length(); i++){
+            char ch = word.charAt(i);
+
+            int idx = map.get(ch);
+            cost+=Math.abs(prevIdx-idx);
+
+            prevIdx = idx;
+        }
+
+        return cost;
+    }
+}
