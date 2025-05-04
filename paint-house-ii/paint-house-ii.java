a4f5924bbd24533bd8ebed048e173e1cde2601ce
rana782 <ranajnv782@gmail.com>
2025-05-04 09:23:37 +0000

    LeetCode: Paint House II (paint-house-ii) — solved on 2025-05-04T09:23:37Z

--- paint-house-ii/paint-house-ii.java
+++ paint-house-ii/paint-house-ii.java
@@ -1,6 +1,41 @@
-# Placeholder for Paint House II (paint-house-ii)
+// LeetCode: Paint House II (paint-house-ii)
+// Submission ID: 1625135964
+// Language: java
+// Timestamp (UTC): 2025-05-04T09:23:37Z
 
-- **Submission ID:** 1625135964
-- **Original solve time (UTC):** 2025-05-04T09:23:37Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int minCostII(int[][] costs) {
+        int house = costs.length;
+        int clr = costs[0].length;
+
+        int matrix[][] = new int[clr][house];
+
+        for(int j = 0; j<house; j++){
+            for(int i = 0; i<clr; i++){
+
+                if(j == 0){
+                    matrix[i][j] = costs[j][i];
+                }
+                else{
+                    int mini = Integer.MAX_VALUE;
+
+                    for(int prevClr = 0; prevClr<clr; prevClr++){
+                        if(prevClr!=i){
+                            mini = Math.min(mini, matrix[prevClr][j-1]);
+                        }
+                    }
+
+                    matrix[i][j] = mini+costs[j][i];
+                }
+            }
+        }
+
+        int res = Integer.MAX_VALUE;
+
+        for(int i = 0; i<clr; i++){
+            res = Math.min(res,matrix[i][house-1]);
+        }
+
+        return res;
+    }
+}
