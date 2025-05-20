71d9260a88b1b425c885c96b01fd48f23a27ebf4
rana782 <ranajnv782@gmail.com>
2025-05-20 13:17:41 +0000

    LeetCode: Data Stream as Disjoint Intervals (data-stream-as-disjoint-intervals) — placeholder (solved on 2025-05-20T13:17:41Z)

--- data-stream-as-disjoint-intervals/data-stream-as-disjoint-intervals.java
+++ data-stream-as-disjoint-intervals/data-stream-as-disjoint-intervals.java
@@ -1,55 +1,6 @@
-// LeetCode: Data Stream as Disjoint Intervals (data-stream-as-disjoint-intervals)
-// Submission ID: 1639315112
-// Language: java
-// Timestamp (UTC): 2025-05-20T13:17:42Z
+# Placeholder for Data Stream as Disjoint Intervals (data-stream-as-disjoint-intervals)
 
-class SummaryRanges {
-    static List<Integer>nums;
-    static HashSet<Integer>set;
-    public SummaryRanges() {
-        nums = new ArrayList<>();
-        set = new HashSet<>();
-    }
-    
-    public void addNum(int value) {
-        if(!set.contains(value)){
-                      nums.add(value);
-                      set.add(value);
-        }
-      
-    }
-    
-    public int[][] getIntervals() {
-        List<List<Integer>>output = new ArrayList<>();
-        int i = 0;
-        Collections.sort(nums);
-        while(i<nums.size()){
-            int j = i;
-
-            while(j+1<nums.size() && nums.get(j)+1 == nums.get(j+1))j++;
-
-            output.add(Arrays.asList(nums.get(i),nums.get(j)));
-            i = j+1;
-        }
-
-        int len = output.size();
-
-        int res[][] = new int[len][2];
-
-        int idx = 0;
-        for(List<Integer>subAns : output){
-            res[idx][0] = subAns.get(0);
-            res[idx][1] = subAns.get(1);
-            idx++;
-        }
-
-        return res;
-    }
-}
-
-/**
- * Your SummaryRanges object will be instantiated and called as such:
- * SummaryRanges obj = new SummaryRanges();
- * obj.addNum(value);
- * int[][] param_2 = obj.getIntervals();
- */
+- **Submission ID:** 1639315112
+- **Original solve time (UTC):** 2025-05-20T13:17:41Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
