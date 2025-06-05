22d5255847499b47b911808d043de18bbb449423
rana782 <ranajnv782@gmail.com>
2025-06-05 18:19:39 +0000

    LeetCode: Odd Even Linked List (odd-even-linked-list) — solved on 2025-06-05T18:19:39Z

--- odd-even-linked-list/odd-even-linked-list.java
+++ odd-even-linked-list/odd-even-linked-list.java
@@ -1,6 +1,60 @@
-# Placeholder for Odd Even Linked List (odd-even-linked-list)
+// LeetCode: Odd Even Linked List (odd-even-linked-list)
+// Submission ID: 1655010986
+// Language: java
+// Timestamp (UTC): 2025-06-05T18:19:39Z
 
-- **Submission ID:** 1655010986
-- **Original solve time (UTC):** 2025-06-05T18:19:38Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null || head.next.next == null)return head;
+        
+        ListNode dummyEven = new ListNode(-1);
+        ListNode tailEven = dummyEven;
+        
+         ListNode dummyOdd = new ListNode(-1);
+        ListNode tailOdd = dummyOdd;
+        
+        ListNode temp = head;
+        
+        int count = 1;
+        
+        while(temp!=null){
+            if(count%2 == 0){
+                ListNode currNode = new ListNode(temp.val);
+                tailEven.next = currNode;
+                tailEven = tailEven.next;
+            }
+            else{
+                ListNode currNode = new ListNode(temp.val);
+                tailOdd.next = currNode;
+                tailOdd = tailOdd.next;
+            }
+            temp =temp.next;
+            count++;
+        }
+        
+        
+        tailOdd.next = dummyEven.next;
+        
+        return dummyOdd.next;
+       
+        
+        
+        
+      
+        
+        
+     
+       
+        
+    }
+}
