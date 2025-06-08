65f512f7c34572f82c75fd12868366357e4f4b0a
rana782 <ranajnv782@gmail.com>
2025-06-08 16:11:38 +0000

    LeetCode: Maximum Size Subarray Sum Equals k (maximum-size-subarray-sum-equals-k) — solved on 2025-06-08T16:11:38Z

--- maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.java
+++ maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.java
@@ -1,6 +1,33 @@
-# Placeholder for Maximum Size Subarray Sum Equals k (maximum-size-subarray-sum-equals-k)
+// LeetCode: Maximum Size Subarray Sum Equals k (maximum-size-subarray-sum-equals-k)
+// Submission ID: 1657814004
+// Language: java
+// Timestamp (UTC): 2025-06-08T16:11:38Z
 
-- **Submission ID:** 1657814004
-- **Original solve time (UTC):** 2025-06-08T16:11:38Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int maxSubArrayLen(int[] nums, int k) {
+        int output = 0;
+        HashMap<Integer,Integer>map = new HashMap<>();
+        map.put(0,-1);
+        
+        int currSum = 0;
+        
+        for(int i = 0; i<nums.length; i++){
+            currSum+=nums[i];
+             
+            
+            if(map.containsKey(currSum-k)){
+                int len = i-map.get(currSum-k);
+                output = Math.max(output,len);
+            }
+            
+            if(!map.containsKey(currSum))map.put(currSum,i);
+            
+              
+            
+               
+            
+        }
+        
+        return output;
+    }
+}
