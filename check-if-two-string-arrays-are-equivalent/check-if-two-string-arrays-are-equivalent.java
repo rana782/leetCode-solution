5d51f5fbccbf9b28f627092658a985552161dc54
rana782 <ranajnv782@gmail.com>
2025-06-08 19:30:24 +0000

    LeetCode: Check If Two String Arrays are Equivalent (check-if-two-string-arrays-are-equivalent) — solved on 2025-06-08T19:30:24Z

--- check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,6 +1,23 @@
-# Placeholder for Check If Two String Arrays are Equivalent (check-if-two-string-arrays-are-equivalent)
+// LeetCode: Check If Two String Arrays are Equivalent (check-if-two-string-arrays-are-equivalent)
+// Submission ID: 1657980800
+// Language: java
+// Timestamp (UTC): 2025-06-08T19:30:24Z
 
-- **Submission ID:** 1657980800
-- **Original solve time (UTC):** 2025-06-08T19:30:23Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder str1 = new StringBuilder();
+        StringBuilder str2 = new StringBuilder();
+        
+        for(String str : word1){
+            str1.append(str);
+        }
+        
+        for(String str : word2){
+            str2.append(str);
+        }
+         System.out.println(str1);
+        System.out.println(str2);
+        
+       return str1.toString().equals(str2.toString());
+    }
+}
