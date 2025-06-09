37147b02b40e9a14f266d36d6f66de3ae83d08ba
rana782 <ranajnv782@gmail.com>
2025-06-09 20:10:17 +0000

    LeetCode: Intersection of Two Arrays (intersection-of-two-arrays) — solved on 2025-06-09T20:10:17Z

--- intersection-of-two-arrays/intersection-of-two-arrays.java
+++ intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,6 +1,36 @@
-# Placeholder for Intersection of Two Arrays (intersection-of-two-arrays)
+// LeetCode: Intersection of Two Arrays (intersection-of-two-arrays)
+// Submission ID: 1659047249
+// Language: java
+// Timestamp (UTC): 2025-06-09T20:10:17Z
 
-- **Submission ID:** 1659047249
-- **Original solve time (UTC):** 2025-06-09T20:10:16Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer>set1 = new HashSet<>();
+        HashSet<Integer>set2 = new HashSet<>();
+        
+        for(int ele : nums1){
+            set1.add(ele);
+        }
+        
+        for(int ele : nums2){
+            set2.add(ele);
+        }
+        
+        List<Integer>res = new ArrayList<>();
+        
+        for(int ele : set1){
+            if(set2.contains(ele))res.add(ele);
+        }
+        
+        
+        System.out.println(res);
+        
+        int output[] = new int[res.size()];
+        
+        for(int i = 0; i<output.length; i++){
+            output[i] = res.get(i);
+        }
+        
+        return output;
+    }
+}
