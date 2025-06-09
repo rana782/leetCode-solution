494a63303f2e41b25295701f7aaaeff75a1ff6ce
rana782 <ranajnv782@gmail.com>
2025-06-09 17:58:58 +0000

    LeetCode: How Many Apples Can You Put into the Basket (how-many-apples-can-you-put-into-the-basket) — solved on 2025-06-09T17:58:58Z

--- how-many-apples-can-you-put-into-the-basket/how-many-apples-can-you-put-into-the-basket.java
+++ how-many-apples-can-you-put-into-the-basket/how-many-apples-can-you-put-into-the-basket.java
@@ -1,6 +1,24 @@
-# Placeholder for How Many Apples Can You Put into the Basket (how-many-apples-can-you-put-into-the-basket)
+// LeetCode: How Many Apples Can You Put into the Basket (how-many-apples-can-you-put-into-the-basket)
+// Submission ID: 1658934871
+// Language: java
+// Timestamp (UTC): 2025-06-09T17:58:58Z
 
-- **Submission ID:** 1658934871
-- **Original solve time (UTC):** 2025-06-09T17:58:57Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int maxNumberOfApples(int[] weight) {
+        int currWt = 0;
+        Arrays.sort(weight);
+        
+        int count = 0;
+        
+        for(int wt : weight){
+            if((currWt+=wt)<=5000){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        
+        return count;
+    }
+}
