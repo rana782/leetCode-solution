25582edd04e1aaf222a2e58a99a4aa5d89addd0f
rana782 <ranajnv782@gmail.com>
2025-06-10 02:54:43 +0000

    LeetCode: Sort Array By Parity (sort-array-by-parity) — solved on 2025-06-10T02:54:43Z

--- sort-array-by-parity/sort-array-by-parity.java
+++ sort-array-by-parity/sort-array-by-parity.java
@@ -1,6 +1,31 @@
-# Placeholder for Sort Array By Parity (sort-array-by-parity)
+// LeetCode: Sort Array By Parity (sort-array-by-parity)
+// Submission ID: 1659233786
+// Language: java
+// Timestamp (UTC): 2025-06-10T02:54:43Z
 
-- **Submission ID:** 1659233786
-- **Original solve time (UTC):** 2025-06-10T02:54:42Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        List<Integer>even = new ArrayList<>();
+        List<Integer>odd = new ArrayList<>();
+        
+        for(int ele : nums){
+            if(ele%2 == 0)even.add(ele);
+            else{
+                odd.add(ele);
+            }
+        }
+        
+        int output[] = new int[nums.length];
+        int idx = 0;
+        
+        for(int ele : even){
+            output[idx++] = ele;
+        }
+        
+        for(int ele : odd){
+            output[idx++] = ele;
+        }
+        
+        return output;
+    }
+}
