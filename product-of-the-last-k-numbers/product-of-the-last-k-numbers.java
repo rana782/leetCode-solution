f101d84063f48927737f9fa0657e30c798c63f4d
rana782 <ranajnv782@gmail.com>
2025-06-10 02:50:34 +0000

    LeetCode: Product of the Last K Numbers (product-of-the-last-k-numbers) — solved on 2025-06-10T02:50:34Z

--- product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
+++ product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
@@ -1,6 +1,34 @@
-# Placeholder for Product of the Last K Numbers (product-of-the-last-k-numbers)
+// LeetCode: Product of the Last K Numbers (product-of-the-last-k-numbers)
+// Submission ID: 1659231188
+// Language: java
+// Timestamp (UTC): 2025-06-10T02:50:34Z
 
-- **Submission ID:** 1659231188
-- **Original solve time (UTC):** 2025-06-10T02:50:33Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class ProductOfNumbers {
+
+    static List<Integer>items;
+    public ProductOfNumbers() {
+        items = new ArrayList<>();
+    }
+    
+    public void add(int num) {
+        items.add(num);
+    }
+    
+    public int getProduct(int k) {
+        int output = 1;
+        int lastIdx = items.size()-1;
+        while(k-->0){
+            output*=items.get(lastIdx);
+            lastIdx--;
+        }
+        
+        return output;
+    }
+}
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers obj = new ProductOfNumbers();
+ * obj.add(num);
+ * int param_2 = obj.getProduct(k);
+ */
