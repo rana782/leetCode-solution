185e7ebda79dd34b511f1ee2a0b0e5c7038fbb21
rana782 <ranajnv782@gmail.com>
2025-06-10 06:03:12 +0000

    LeetCode: Excel Sheet Column Number (excel-sheet-column-number) — solved on 2025-06-10T06:03:12Z

--- excel-sheet-column-number/excel-sheet-column-number.java
+++ excel-sheet-column-number/excel-sheet-column-number.java
@@ -1,6 +1,24 @@
-# Placeholder for Excel Sheet Column Number (excel-sheet-column-number)
+// LeetCode: Excel Sheet Column Number (excel-sheet-column-number)
+// Submission ID: 1659395713
+// Language: java
+// Timestamp (UTC): 2025-06-10T06:03:12Z
 
-- **Submission ID:** 1659395713
-- **Original solve time (UTC):** 2025-06-10T06:03:12Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        StringBuilder str = new StringBuilder(columnTitle);
+        str.reverse();
+        System.out.println(str);
+        
+        
+        
+       int output = 0;
+        
+        for(int i = 0; i<str.length(); i++){
+            int code  = str.charAt(i)-'A'+1;
+            
+            output+=Math.pow(26,i)*code;
+        }
+        
+        return output;
+    }
+}
