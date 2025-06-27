8e6feaf1e5f10e37bc58c242dd1e35380b782558
rana782 <ranajnv782@gmail.com>
2025-06-27 08:36:18 +0000

    LeetCode: How Many Numbers Are Smaller Than the Current Number (how-many-numbers-are-smaller-than-the-current-number) — placeholder (solved on 2025-06-27T08:36:18Z)

--- how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,32 +1,6 @@
-// LeetCode: How Many Numbers Are Smaller Than the Current Number (how-many-numbers-are-smaller-than-the-current-number)
-// Submission ID: 1678097068
-// Language: java
-// Timestamp (UTC): 2025-06-27T08:36:19Z
+# Placeholder for How Many Numbers Are Smaller Than the Current Number (how-many-numbers-are-smaller-than-the-current-number)
 
-class Solution {
-    public int[] smallerNumbersThanCurrent(int[] nums) {
-        HashMap<Integer,Integer>map = new HashMap<>();
-        
-        int length = nums.length;
-        
-        int output[] = new int[length];
-        
-        for(int i = 0; i<nums.length; i++){
-            if(map.containsKey(nums[i])){
-                output[i] = map.get(nums[i]);
-                continue;
-            }
-            
-            int count = 0;
-            
-            for(int j = 0; j<nums.length; j++){
-                if(j!=i && nums[j]<nums[i])count++;
-            }
-            
-            output[i] = count;
-            map.put(nums[i],count);
-        }
-        
-        return output;
-    }
-}
+- **Submission ID:** 1678097068
+- **Original solve time (UTC):** 2025-06-27T08:36:18Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
