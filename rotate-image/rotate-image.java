978202f764bd7f8bd1ee380a91d7a7e872eb4d61
rana782 <ranajnv782@gmail.com>
2025-07-07 14:24:55 +0000

    LeetCode: Rotate Image (rotate-image) — placeholder (solved on 2025-07-07T14:24:55Z)

--- rotate-image/rotate-image.java
+++ rotate-image/rotate-image.java
@@ -1,36 +1,6 @@
-// LeetCode: Rotate Image (rotate-image)
-// Submission ID: 1689645468
-// Language: java
-// Timestamp (UTC): 2025-07-07T14:24:55Z
+# Placeholder for Rotate Image (rotate-image)
 
-class Solution {
-    static int n;
-    public static void transpose(int mat[][]){
-      for(int i = 0; i<n; i++){
-          for(int j = i+1; j<n; j++){
-              int temp = mat[i][j];
-              
-              mat[i][j] = mat[j][i];
-              mat[j][i] = temp;
-              
-          }
-      }
-    }
-    
-    public static void mirror(int mat[][]){
-        
-        for(int i =0; i<n; i++){
-            for(int j = 0; j<n/2; j++){
-                
-                int temp = mat[i][j];
-                mat[i][j] = mat[i][n-j-1];
-                mat[i][n-j-1] = temp;
-            }
-        }
-    }
-    public void rotate(int[][] matrix) {
-        n = matrix.length;
-        transpose(matrix);
-        mirror(matrix);
-    }
-}
+- **Submission ID:** 1689645468
+- **Original solve time (UTC):** 2025-07-07T14:24:55Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
