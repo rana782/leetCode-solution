5fde20df59d0a794792ac73bd21c405065500ebc
rana782 <ranajnv782@gmail.com>
2025-07-07 20:28:28 +0000

    LeetCode: Remove Nth Node From End of List (remove-nth-node-from-end-of-list) — placeholder (solved on 2025-07-07T20:28:28Z)

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,47 +1,6 @@
-// LeetCode: Remove Nth Node From End of List (remove-nth-node-from-end-of-list)
-// Submission ID: 1690047227
-// Language: java
-// Timestamp (UTC): 2025-07-07T20:28:29Z
+# Placeholder for Remove Nth Node From End of List (remove-nth-node-from-end-of-list)
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        int length = 0;
-        
-        ListNode temp = head;
-        
-        while(temp!=null){
-            length++;
-            temp = temp.next;
-        }
-        
-        
-        int pos = length-n;
-        
-        int currPos = 1;
-        
-        if(pos == 0)return head.next;
-        
-        ListNode back = head;
-        ListNode front = head.next;
-        
-        while(currPos!=pos){
-            back = back.next;
-            front = front.next;
-            currPos++;
-        }
-        
-        back.next = front.next;
-        return head;
-        
-    }
-}
+- **Submission ID:** 1690047227
+- **Original solve time (UTC):** 2025-07-07T20:28:28Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
