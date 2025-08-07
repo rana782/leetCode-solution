5ac232c6fd4cedbc720dff1d849ce219ada22a37
rana782 <ranajnv782@gmail.com>
2025-08-07 18:20:39 +0000

    LeetCode: Majority Element (majority-element) — solved on 2025-08-07T18:20:39Z

--- majority-element/majority-element.java
+++ majority-element/majority-element.java
@@ -1,6 +1,26 @@
-# Placeholder for Majority Element (majority-element)
+// LeetCode: Majority Element (majority-element)
+// Submission ID: 1727215867
+// Language: java
+// Timestamp (UTC): 2025-08-07T18:20:39Z
 
-- **Submission ID:** 1727215867
-- **Original solve time (UTC):** 2025-08-07T18:20:39Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int majorityElement(int[] nums) {
+        int curr_ele = nums[0];
+        int curr_len = 1;
+
+        for(int i = 1; i<nums.length; i++){
+            if(curr_len == 0){
+                curr_ele = nums[i];
+                curr_len = 1;
+            }
+            else{
+                if(nums[i] == curr_ele)curr_len++;
+                else{
+                    curr_len--;
+                }
+            }
+        }
+
+        return curr_ele;
+    }
+}
