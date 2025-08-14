8c419267e798ebee2feff9248adbc18abf592527
rana782 <ranajnv782@gmail.com>
2025-08-14 18:17:45 +0000

    LeetCode: Group Anagrams (group-anagrams) — solved on 2025-08-14T18:17:45Z

--- group-anagrams/group-anagrams.java
+++ group-anagrams/group-anagrams.java
@@ -1,6 +1,33 @@
-# Placeholder for Group Anagrams (group-anagrams)
+// LeetCode: Group Anagrams (group-anagrams)
+// Submission ID: 1735271623
+// Language: java
+// Timestamp (UTC): 2025-08-14T18:17:45Z
 
-- **Submission ID:** 1735271623
-- **Original solve time (UTC):** 2025-08-14T18:17:44Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>>map = new  HashMap<>();
+
+
+        for(String str : strs){
+
+            int arr[] = new int[26];
+
+            for(char ch : str.toCharArray()){
+                arr[ch-'a']++;
+            }
+
+            String code = Arrays.toString(arr);
+            List<String>subList = map.getOrDefault(code,new ArrayList<>());
+            subList.add(str);
+            map.put(code,subList);
+        }
+
+        List<List<String>>output = new ArrayList<>();
+
+        for(List<String>subAns : map.values()){
+            output.add(subAns);
+        }
+
+        return output;
+    }
+}
