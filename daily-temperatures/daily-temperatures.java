0bc9cbc33946fb9ed0011be33514c047299d8468
rana782 <ranajnv782@gmail.com>
2025-08-15 14:21:30 +0000

    LeetCode: Daily Temperatures (daily-temperatures) — solved on 2025-08-15T14:21:30Z

--- daily-temperatures/daily-temperatures.java
+++ daily-temperatures/daily-temperatures.java
@@ -1,6 +1,28 @@
-# Placeholder for Daily Temperatures (daily-temperatures)
+// LeetCode: Daily Temperatures (daily-temperatures)
+// Submission ID: 1736207680
+// Language: java
+// Timestamp (UTC): 2025-08-15T14:21:30Z
 
-- **Submission ID:** 1736207680
-- **Original solve time (UTC):** 2025-08-15T14:21:29Z
-- **Note:** LeetCode API did not return code for this submission.
-  This placeholder ensures a backdated commit for your contribution graph.
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer>st = new Stack<>();
+        int n = temperatures.length;
+         int output[] = new int[n];
+         int i = n-1;
+
+         while(i>=0){
+            int temp = temperatures[i];
+            while(!st.isEmpty() && temperatures[st.peek()]<=temp)st.pop();
+            if(st.size() == 0)output[i] = 0;
+            else{
+                int day = st.peek()-i;
+                output[i] = day;
+            }
+            st.push(i);
+
+            i--;
+         }
+
+         return output;
+    }
+}
